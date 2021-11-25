8336296ac3b45d563909263ca33971ced7407ea5
collins <collinskipkoech95@gmail.com>
2021-11-25 21:22:58 +0300

    created test class with a testLogin method to check  login functionality test

---
+++ src/test/java/com/cheatSheet/tests/LoginTest.java
@@ -0,0 +1,12 @@
+package com.cheatSheet.tests;
+
+import com.cheatSheet.utility.TestBase;
+import org.junit.jupiter.api.Test;
+
+public class LoginTest extends TestBase {
+
+    @Test
+    public void testLogin(){
+        
+    }
+}
